0b73dac555181181a928101695fdb15863e2b45e
hyperoot <hyperoot.tech@proton.me>
2024-08-07 11:27:12 +0000

    multiple changes

--- followsphere/__main__.py
+++ followsphere/__main__.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import typer
 from beaupy import select
 from rich.console import Console
@@ -7,17 +9,17 @@ from followsphere.utils import install_playwright_browsers, show_banner
 
 
 def main():
-    console = Console()
+    console: Console = Console()
     console.clear()
     install_playwright_browsers()
     show_banner()
-    names = [
+    options: List[str] = [
         "Linkedin",
         "Instagram (Coming soon)",
     ]
-    console.print("Choose your platform")
-    name: str = select(sorted(names), cursor="🢧", cursor_style="red")
-    if name.lower() == "linkedin":
+    console.print("Pick a platform (Use arrow keys)\n", style="Cyan")
+    option: str = select(sorted(options), cursor="\uf061", cursor_style="red")
+    if option.lower() == "linkedin":
         typer.run(execute_linkedin)
     else:
         typer.echo("\nInvalid platform selected.")
--- followsphere/utils.py
+++ followsphere/utils.py
@@ -2,21 +2,23 @@ import json
 import os
 import subprocess
 import sys
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
+import toml
 from beaupy import select_multiple
 from pyfiglet import Figlet
 from rich.console import Console
+from rich.panel import Panel
 from termcolor import cprint
 
 
-def install_playwright_browsers():
-    console = Console()
+def install_playwright_browsers() -> None:
+    console: Console = Console()
     try:
-        from playwright.sync_api import sync_playwright
+        from playwright.sync_api import Browser, sync_playwright
 
         with sync_playwright() as p:
-            browser = p.chromium.launch()
+            browser: Browser = p.chromium.launch()
             browser.close()
     except Exception as e:
         print(f"Error: {e}")
@@ -26,20 +28,34 @@ def install_playwright_browsers():
     console.clear()
 
 
-def show_banner():
+def get_version_from_toml() -> str:
     try:
-        width = os.get_terminal_size()[0]
+        pyproject: Dict[str, Any] = toml.load("pyproject.toml")
+        return pyproject.get("tool", {}).get("poetry", {}).get("version", "unknown")
+    except Exception as e:
+        print(f"Error reading version from pyproject.toml: {e}")
+        return "unknown"
+
+
+def show_banner() -> None:
+    try:
+        width: int = os.get_terminal_size()[0]
     except Exception:
         width = 80
 
-    banner = Figlet(font="big", justify="left", width=width)
+    banner: Figlet = Figlet(font="big", justify="center", width=width)
     cprint(banner.renderText("FollowSphere"), color="blue")
+
+    version: str = get_version_from_toml()
+    url: str = "https://hyperoot.dev"
+    console: Console = Console()
+    console.print(Panel(f"{url} - v{version}"), justify="center", style="green")
 
 
 def read_data() -> Optional[Dict]:
     try:
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        data_file_path = os.path.join(script_dir, "data.json")
+        script_dir: str = os.path.dirname(os.path.abspath(__file__))
+        data_file_path: str = os.path.join(script_dir, "data.json")
         with open(data_file_path, "r") as file:
             data: Dict = json.load(file)
         return data
@@ -48,19 +64,21 @@ def read_data() -> Optional[Dict]:
         exit()
 
 
-def tags_follower(data: Dict):
-    console = Console()
+def tags_follower(data: Dict[str, Any]) -> Optional[List[str]]:
+    console: Console = Console()
     console.clear()
     show_banner()
-    options: List = []
+    options: List[str] = []
     for k, _v in data.items():
         options.append(k)
 
-    console.print("Which tag collection do you want to follow?", style="red")
-    items = select_multiple(sorted(options), tick_character="✓", ticked_indices=None, tick_style="green")
+    console.print("Which tag collection do you want to follow?\n", style="Cyan")
+    items: List[Union[int, Any]] = select_multiple(
+        sorted(options), tick_character="✓", ticked_indices=None, tick_style="green"
+    )
     if items:
-        console.print(f"\nYou selected {items}")
-        tags = []
+        console.print(f"\nSelected tag collections: [green]{" ".join(items)}[/green]")
+        tags: List[str] = []
         for item in items:
             if data[item]["tags"]:
                 for tag in data[item]["tags"]:
